06302047d87831d0b0a129c94260d16969dee877
Neighborpil <neighborpil@naver.com>
2022-07-11 09:34:38 +0900

    save

--- reactive-spring-webflux/reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-spring-webflux/reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -8,12 +8,32 @@ import java.util.List;
 public class FluxAndMonoGeneratorService {
 
     public Flux<String> namesFlux() {
-        return Flux.fromIterable(List.of("alex", "ben", "chloe")); // db or remote service call
+        return Flux.fromIterable(List.of("alex", "ben", "chloe"))
+            .log(); // db or remote service call
     }
 
     public Mono<String> nameMono() {
 
-        return Mono.just("alex");
+        return Mono.just("alex").log();
+    }
+
+    public Flux<String> namesFlux_map(int stringLength) {
+
+        // filter the string whose length is greater than 3
+        return Flux.fromIterable(List.of("alex", "ben", "chloe"))
+            .map(String::toUpperCase)
+//            .map(s -> s.toUpperCase())
+            .filter(s -> s.length() > stringLength)
+            .map(s -> s.length() + "-" + s)
+            .log(); // db or remote service call
+    }
+
+    public Flux<String> namesFlux_immutability() {
+
+        var naemsFlux = Flux.fromIterable(List.of("alex", "ben", "chloe"));
+        naemsFlux.map(String::toUpperCase); // flux is immutable
+        return naemsFlux;
+
     }
 
     public static void main(String[] args) {
--- reactive-spring-webflux/reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-spring-webflux/reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -2,6 +2,49 @@ package com.learnreactiveprogramming.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
 class FluxAndMonoGeneratorServiceTest {
 
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+    @Test
+    void namesFlux() {
+
+        Flux<String> namesFlux = fluxAndMonoGeneratorService.namesFlux();
+
+        StepVerifier.create(namesFlux)
+//            .expectNext("alex", "ben", "chloe")
+//            .expectNextCount(3)
+            .expectNext("alex")
+            .expectNextCount(2)
+            .verifyComplete();
+
+    }
+
+    @Test
+    void namesFlux_map() {
+
+        int stringLength = 3;
+
+        Flux<String> namesFlux = fluxAndMonoGeneratorService.namesFlux_map(stringLength);
+
+        StepVerifier.create(namesFlux)
+            .expectNext("4-ALEX", "5-CHLOE")
+            .verifyComplete();
+
+    }
+
+    @Test
+    void namesFlux_immutability() {
+
+        Flux<String> namesFlux = fluxAndMonoGeneratorService.namesFlux_immutability();
+
+        StepVerifier.create(namesFlux)
+            .expectNext("alex", "ben", "chloe")
+            .verifyComplete();
+
+    }
 }
