4425ec730079a28e72791044c27c817c49d75f91
neighborpil <neighborpil@naver.com>
2022-07-20 09:40:01 +0900

    save

--- reactive-spring-webflux/movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
+++ reactive-spring-webflux/movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
@@ -2,12 +2,16 @@ package com.reactivespring.client;
 
 import com.reactivespring.config.WebClientConfig;
 import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.exception.MoviesInfoClientException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Component
+@Slf4j
 public class MoviesInfoRestClient {
 
     private WebClient webClient;
@@ -26,6 +30,26 @@ public class MoviesInfoRestClient {
             .get()
             .uri(url, movieId)
             .retrieve()
+            .onStatus(HttpStatus::is4xxClientError, clientResponse -> {
+                log.info("Status code is : {}", clientResponse.statusCode().value());
+                if (clientResponse.statusCode().equals(HttpStatus.NOT_FOUND)) {
+                    return Mono.error(new MoviesInfoClientException(
+                        "There is no MovieInfo Avaialable for the passed in Id : " + movieId, clientResponse.statusCode().value()));
+                }
+
+                return clientResponse.bodyToMono(String.class)
+                    .flatMap(responseMessage -> Mono.error(new MoviesInfoClientException(
+                        responseMessage, clientResponse.statusCode().value()
+                    )));
+            })
+            .onStatus(HttpStatus::is5xxServerError, clientResponse -> {
+                log.info("Status code is : {}", clientResponse.statusCode().value());
+
+                return clientResponse.bodyToMono(String.class)
+                    .flatMap(responseMessage -> Mono.error(new MoviesInfoClientException(
+                        "Server Exception in MoviesInfoService " + responseMessage, clientResponse.statusCode().value()
+                    )));
+            })
             .bodyToMono(MovieInfo.class)
             .log();
     }
--- reactive-spring-webflux/movies-service/src/main/java/com/reactivespring/controller/MoviesController.java
+++ reactive-spring-webflux/movies-service/src/main/java/com/reactivespring/controller/MoviesController.java
@@ -1,6 +1,10 @@
 package com.reactivespring.controller;
 
+import com.reactivespring.client.MoviesInfoRestClient;
+import com.reactivespring.client.ReviewsRestClient;
 import com.reactivespring.domain.Movie;
+import com.reactivespring.domain.Review;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +15,24 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/v1/movies")
 public class MoviesController {
 
+    private MoviesInfoRestClient moviesInfoRestClient;
+    private ReviewsRestClient reviewsRestClient;
+
+    public MoviesController(MoviesInfoRestClient moviesInfoRestClient, ReviewsRestClient reviewsRestClient) {
+        this.moviesInfoRestClient = moviesInfoRestClient;
+        this.reviewsRestClient = reviewsRestClient;
+    }
+
     @GetMapping("/{id}")
     public Mono<Movie> retrieveMovieById(@PathVariable("id") String movieId) {
 
-        return null;
+        return moviesInfoRestClient.retrieveMovieInfo(movieId)
+            .flatMap(movieInfo -> {
+                Mono<List<Review>> reviewsListMono = reviewsRestClient.retrieveReviews(movieId)
+                    .collectList();
+
+                return reviewsListMono.map(reviews -> new Movie(movieInfo, reviews));
+            });
     }
 
 }
